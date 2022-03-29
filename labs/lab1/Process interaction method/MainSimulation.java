1c1c49ca9b1096b272ed1150e8541616c2afe503
Zoom/comp <ax2685mu-s@student.lu.se>
2022-03-29 12:26:13 +0200

    Lab1

--- labs/lab1/Process interaction method/MainSimulation.java
+++ labs/lab1/Process interaction method/MainSimulation.java
@@ -1,51 +1,70 @@
 import java.util.*;
 import java.io.*;
 
-//Denna klass ärver Global så att man kan använda time och signalnamnen utan punktnotation
+//Denna klass rver Global s att man kan anvnda time och signalnamnen utan punktnotation
 //It inherits Proc so that we can use time and the signal names without dot notation
 
+public class MainSimulation extends Global {
 
-public class MainSimulation extends Global{
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
+		// Signallistan startas och actSignal deklareras. actSignal r den senast
+		// utplockade signalen i huvudloopen nedan.
+		// The signal list is started and actSignal is declaree. actSignal is the latest
+		// signal that has been fetched from the
+		// signal list in the main loop below.
 
-    	//Signallistan startas och actSignal deklareras. actSignal är den senast utplockade signalen i huvudloopen nedan.
-    	// The signal list is started and actSignal is declaree. actSignal is the latest signal that has been fetched from the 
-    	// signal list in the main loop below.
+		Signal actSignal;
+		new SignalList();
 
-    	Signal actSignal;
-    	new SignalList();
+		// Hï¿½r nedan skapas de processinstanser som behÃ¶vs och parametrar i dem ges
+		// vÃ¤rden.
+		// Here process instances are created (two queues and one generator) and their
+		// parameters are given values.
 
-    	//Här nedan skapas de processinstanser som behövs och parametrar i dem ges värden.
-    	// Here process instances are created (two queues and one generator) and their parameters are given values. 
+		QS Q1 = new QS();
+		QS Q2 = new QS();
+		// QS Q3 = new QS();
+		Q1.sendTo = Q2;
+		Q2.sendTo = null;
+		// Q3.sendTo = null;
 
-    	QS Q1 = new QS();
-    	Q1.sendTo = null;
+		Gen Generator = new Gen();
+		Generator.lambda = 9; // Generator ska generera nio kunder per sekund //Generator shall generate 9
+								// customers per second
+		Generator.sendTo = Q1; // De lgenererade kunderna ska skickas til kÃ¶systemet QS // The generated
+								// customers shall be sent to Q1
 
-    	Gen Generator = new Gen();
-    	Generator.lambda = 9; //Generator ska generera nio kunder per sekund  //Generator shall generate 9 customers per second
-    	Generator.sendTo = Q1; //De genererade kunderna ska skickas till kösystemet QS  // The generated customers shall be sent to Q1
+		// Hï¿½r nedan skickas de fÃ¶rsta signalerna fï¿½r att simuleringen ska komma igÃ¥ng.
+		// To start the simulation the first signals are put in the signal list
 
-    	//Här nedan skickas de första signalerna för att simuleringen ska komma igång.
-    	//To start the simulation the first signals are put in the signal list
+		SignalList.SendSignal(READY, Generator, time);
+		SignalList.SendSignal(MEASURE, Q1, time);
+		SignalList.SendSignal(MEASURE, Q2, time);
+		// SignalList.SendSignal(MEASURE, Q3, time);
 
-    	SignalList.SendSignal(READY, Generator, time);
-    	SignalList.SendSignal(MEASURE, Q1, time);
+		// Detta ï¿½r simuleringsloopen:
+		// This is the main loop
 
+		while (time < 10000) {
+			// System.out.println("Accuml in Que Q1: " + Q1.accumulated);
+			// System.out.println("Accuml in Que: Q2: " + Q2.accumulated);
+			// System.out.println("Accuml in Que: Q3: " + Q3.accumulated);
+			Q1.sendTo = null;
+			// Q1.sendTo = Q2;
 
-    	// Detta är simuleringsloopen:
-    	// This is the main loop
+			actSignal = SignalList.FetchSignal();
+			time = actSignal.arrivalTime;
+			actSignal.destination.TreatSignal(actSignal);
+		}
 
-    	while (time < 100000){
-    		actSignal = SignalList.FetchSignal();
-    		time = actSignal.arrivalTime;
-    		actSignal.destination.TreatSignal(actSignal);
-    	}
+		// Slutligen skrivs resultatet av simuleringen ut nedan:
+		// Finally the result of the simulation is printed below:
 
-    	//Slutligen skrivs resultatet av simuleringen ut nedan:
-    	//Finally the result of the simulation is printed below:
+		// System.out.println("Mean number of customers in Q1: " + 1.0 * Q1.accumulated / Q1.noMeasurements);
+		// System.out.println("Mean number of customers in Q2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
+		// System.out.println("Mean number of customers in Q3: " +
+		// 1.0*Q3.accumulated/Q3.noMeasurements);
 
-    	System.out.println("Mean number of customers in queuing system: " + 1.0*Q1.accumulated/Q1.noMeasurements);
-
-    }
+	}
 }
