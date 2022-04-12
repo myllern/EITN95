da3c65282e0d9aec8a5e023013e79746ebe20a18
oliver_persson_b <oliver.persson.b@gmail.com>
2022-04-12 14:27:25 +0200

    working solution

--- assignments/assign1/task1/MainSimulation.java
+++ assignments/assign1/task1/MainSimulation.java
@@ -5,16 +5,33 @@ import java.io.*;
 public class MainSimulation extends GlobalSimulation {
 
     public static void main(String[] args) throws IOException {
-        Event currentEvent;
-        State state = new State();
-        insertEvent(ARRIVAL_Q1, 0);
-        insertEvent(MEASURE, sampleTime);
-
-        int N = 1000;
-        while (state.nbrOfMeasurments < N) {
-            currentEvent = eventList.fetchEvent();
-            time = currentEvent.eventTime;
-            state.treatEvent(currentEvent);
-        }
+        part1();
     }
+
+    public static void part1() throws IOException {
+        double meanSum = 0;
+        int Nsims = 100;
+
+        for (int i = 0; i < Nsims; i++) {
+            reset();
+            Event currentEvent;
+            State state = new State();
+            insertEvent(ARRIVAL_Q1, 0);
+            insertEvent(MEASURE, sampleTime);
+
+            int N = 1000;
+            while (state.nbrOfMeasurements < N) {
+                currentEvent = eventList.fetchEvent();
+                time = currentEvent.eventTime;
+                state.treatEvent(currentEvent);
+            }
+
+            double meanQ2 = 1.0 * (double) state.accNbrInQ2 / (double) N;
+            meanSum += meanQ2;
+        }
+
+        System.out.println("Mean for Q2");
+        System.out.println(1.0 * meanSum / (double) Nsims);
+    }
+
 }
--- assignments/assign1/task1/State.java
+++ assignments/assign1/task1/State.java
@@ -5,33 +5,100 @@ import java.util.*;
 
 class State extends GlobalSimulation {
     public int nbrInQ1 = 0;
+    public int nbrOfArrivalsQ1 = 0;
+    public int nbrOfRejectedQ1 = 0;
     public int nbrInQ2 = 0;
     public int accNbrInQ2 = 0;
 
-    public int nbrOfMeasurments = 0;
+    public int nbrOfMeasurements = 0;
 
     public int sizeQ1 = 10;
-    public double arrivalTimeQ1 = 1.0;
+    public double timeBetweenArrivalQ1 = 5;
+    public double meanQ1 = 2.1;
+    public double lamdaQ1 = 1.0 / meanQ1;
+    public double serviceTimeQ2 = 2.0;
 
     Random rand = new Random();
 
     public void treatEvent(Event x) {
         switch (x.eventType) {
             case ARRIVAL_Q1:
+                arrivalQ1();
                 break;
+
+            case SERVED_Q1:
+                servedQ1();
+                break;
+
+            case ARRIVAL_Q2:
+                arrivalQ2();
+                break;
+
+            case SERVED_Q2:
+                servedQ2();
+                break;
+
             case MEASURE:
                 measure();
                 break;
         }
     }
 
+    private void arrivalQ1() {
+        nbrOfArrivalsQ1++;
+
+        if (nbrInQ1 == 0) {
+            nbrInQ1++;
+            insertEvent(SERVED_Q1, time + expDistPdf(lamdaQ1));
+        } else if (nbrInQ1 < 10) {
+            nbrInQ1++;
+        } else {
+            nbrOfRejectedQ1++;
+        }
+
+        insertEvent(ARRIVAL_Q1, time + timeBetweenArrivalQ1);
+    }
+
+    private void servedQ1() {
+        nbrInQ1--;
+        insertEvent(ARRIVAL_Q2, time);
+
+        check();
+        if (nbrInQ1 > 0)
+            insertEvent(SERVED_Q1, time + expDistPdf(lamdaQ1));
+
+    }
+
+    private void arrivalQ2() {
+        if (nbrInQ2 == 0)
+            insertEvent(SERVED_Q2, time + serviceTimeQ2);
+        nbrInQ2++;
+    }
+
+    private void servedQ2() {
+        nbrInQ2--;
+        check();
+        if (nbrInQ2 > 0)
+            insertEvent(SERVED_Q2, time + serviceTimeQ2);
+    }
+
     private void measure() {
-        accNbrInQ2 = accNbrInQ2 + nbrInQ2;
-        nbrOfMeasurments++;
+        accNbrInQ2 += nbrInQ2;
+        nbrOfMeasurements++;
         insertEvent(MEASURE, sampleTime);
+        // System.out.println("------------");
+        // System.out.println("nbr in q1: " + nbrInQ1);
+        // System.out.println("nbr in q2: " + nbrInQ2);
+        // System.out.println("------------");
     }
 
     private double expDistPdf(double lambda) {
         return (-1.0) * Math.log(1 - rand.nextDouble()) / lambda;
+    }
+
+    private void check() {
+        if (nbrInQ1 < 0 || nbrInQ2 < 0) {
+            throw new Error("Negative queues!");
+        }
     }
 }
