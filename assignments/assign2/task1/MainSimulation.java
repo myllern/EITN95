89a148641d392a18b208e4dc60583a4178a47a4e
oliver_persson_b <oliver.persson.b@gmail.com>
2022-04-19 18:01:27 +0200

    state done, next run the simulation

--- assignments/assign2/task1/MainSimulation.java
+++ assignments/assign2/task1/MainSimulation.java
@@ -5,7 +5,11 @@ import java.io.*;
 public class MainSimulation extends GlobalSimulation {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("hello world");
+        part1();
+    }
+
+    public static void part1() throws IOException {
+        System.out.println("hello world2s");
     }
 
 }
--- assignments/assign2/task1/State.java
+++ assignments/assign2/task1/State.java
@@ -9,18 +9,52 @@ class State extends GlobalSimulation {
     public double lambda = 8.0;
     public int T = 1;
     public int M = 1000;
+    public int nbrOfCustomers = 0;
+
+    public int[] ys = new int[M];
+    private int measurment_idx = 0;
+    public boolean isDone = false;
 
     Random rand = new Random();
 
     public void treatEvent(Event x) {
         switch (x.eventType) {
             case MEASURE:
+                measure();
                 break;
             case ARRIVAL:
+                arrival();
                 break;
             case SERVED:
+                served();
                 break;
         }
+    }
+
+    private void arrival() {
+        // a server is free
+        if (nbrOfCustomers < N) {
+            nbrOfCustomers++;
+            insertEvent(SERVED, time + x);
+        }
+
+        insertEvent(ARRIVAL, time + expDistPdf(lambda));
+    }
+
+    private void served() {
+        nbrOfCustomers--;
+    }
+
+    private void measure() {
+        if (measurment_idx < M) {
+            ys[measurment_idx] = nbrOfCustomers;
+            measurment_idx++;
+        }
+
+        isDone = measurment_idx >= M;
+
+        if (!isDone)
+            insertEvent(MEASURE, time + T);
     }
 
     private double expDistPdf(double lambda) {
