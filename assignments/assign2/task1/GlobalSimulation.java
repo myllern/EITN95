afefe365ce5fe4f0092cd3ae6febb91b45fb0775
oliver_persson_b <oliver.persson.b@gmail.com>
2022-04-19 17:44:53 +0200

    defined needed events

--- assignments/assign2/task1/GlobalSimulation.java
+++ assignments/assign2/task1/GlobalSimulation.java
@@ -1,7 +1,11 @@
 package assign2.task1;
 
 public class GlobalSimulation {
+    // EVENTS!
     public static final int MEASURE = 1;
+    public static final int ARRIVAL = 2;
+    public static final int SERVED = 3;
+
     public static double time = 0; // The global time variable
     public static EventListClass eventList = new EventListClass(); // The event list used in the program
 
--- assignments/assign2/task1/State.java
+++ assignments/assign2/task1/State.java
@@ -4,11 +4,21 @@ package assign2.task1;
 import java.util.*;
 
 class State extends GlobalSimulation {
+    public int N = 1000;
+    public int x = 100;
+    public double lambda = 8.0;
+    public int T = 1;
+    public int M = 1000;
+
     Random rand = new Random();
 
     public void treatEvent(Event x) {
         switch (x.eventType) {
             case MEASURE:
+                break;
+            case ARRIVAL:
+                break;
+            case SERVED:
                 break;
         }
     }
