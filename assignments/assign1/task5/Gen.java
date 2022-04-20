2f550b1ea3789fca18268fc6dc8e1b7ebecc43de
Zoom/comp <ax2685mu-s@student.lu.se>
2022-04-20 12:49:07 +0200

    cont on gen in ass 4

--- assignments/assign1/task5/Gen.java
+++ assignments/assign1/task5/Gen.java
@@ -18,45 +18,88 @@ class Gen extends Proc {
 	public int sentArrivals;
 	public int nrOfArrivals;
 
+	int rrNr = 0;
+
 	QS Q1 = new QS();
 	QS Q2 = new QS();
 	QS Q3 = new QS();
 	QS Q4 = new QS();
 	QS Q5 = new QS();
 
+	ArrayList<QS> queueList = null;
+
 	// H�r nedan anger man vad som ska g�ras n�r en signal kommer //What to do when
 	// a signal arrives
 	public void TreatSignal(Signal x) {
 		switch (x.signalType) {
-			// case RANDOM: {
-			// 	if (run) {
-			// 		int dice = rand.nextInt(100);
-			// 		if (dice < 20) {
-			// 			System.out.println("Number in Q1: " + Q1.inQueue.size());
-			// 			SignalList.SendSignal(ARRIVAL, Q1, time);
-			// 		} else if (dice < 40) {
-			// 			System.out.println("Number in Q2: " + Q2.inQueue.size());
-			// 			SignalList.SendSignal(ARRIVAL, Q2, time);
-			// 		} else if (dice < 60) {
-			// 			System.out.println("Number in Q3: " + Q3.inQueue.size());
-			// 			SignalList.SendSignal(ARRIVAL, Q3, time);
-			// 		} else if (dice < 80) {
-			// 			System.out.println("Number in Q4: " + Q4.inQueue.size());
-			// 			SignalList.SendSignal(ARRIVAL, Q4, time);
-			// 		} else {
-			// 			System.out.println("Number in Q5: " + Q5.inQueue.size());
-			// 			SignalList.SendSignal(ARRIVAL, Q5, time);
-			// 		}
-			// 		SignalList.SendSignal(RANDOM, this, time + lambda);
-			// 	}
-			// 	break;
-			// }
+			case RANDOM: {
+				if (run) {
+					int dice = rand.nextInt(100);
+					if (dice < 20) {
+						System.out.println("Number in Q1: " + Q1.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q1, time);
+					} else if (dice < 40) {
+						System.out.println("Number in Q2: " + Q2.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q2, time);
+					} else if (dice < 60) {
+						System.out.println("Number in Q3: " + Q3.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q3, time);
+					} else if (dice < 80) {
+						System.out.println("Number in Q4: " + Q4.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q4, time);
+					} else {
+						System.out.println("Number in Q5: " + Q5.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q5, time);
+					}
+					SignalList.SendSignal(RANDOM, this, time + lambda);
+				}
+				break;
+			}
+
+			case RR: {
+				if (run) {
+					if (rrNr == 1) {
+						System.out.println("Number in Q1: " + Q1.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q1, time);
+						rrNr++;
+					} else if (rrNr == 2) {
+						System.out.println("Number in Q2: " + Q2.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q2, time);
+						rrNr++;
+					} else if (rrNr == 3) {
+						System.out.println("Number in Q3: " + Q3.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q3, time);
+						rrNr++;
+					} else if (rrNr == 4) {
+						System.out.println("Number in Q4: " + Q4.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q4, time);
+					} else {
+						System.out.println("Number in Q5: " + Q5.inQueue.size());
+						SignalList.SendSignal(ARRIVAL, Q5, time);
+						rrNr = 1;
+					}
+					SignalList.SendSignal(RR, this, time + lambda);
+				}
+				break;
+			}
+
+			case LOW: {
+				if (queueList == null) {
+					queueList = new ArrayList<QS>();
+					queueList.add(Q1);
+					queueList.add(Q2);
+					queueList.add(Q3);
+					queueList.add(Q4);
+					queueList.add(Q5);
+				}
+
+			}
 			case SINGLE: {
 				if (sentArrivals < nrOfArrivals && run) {
 					SignalList.SendSignal(ARRIVAL, Q1, time);
 					sentArrivals++;
 					SignalList.SendSignal(SINGLE, this, time + lambda);
-				}else{
+				} else {
 
 					SignalList.SendSignal(LAST_ARRIVAL, Q1, time);
 				}
@@ -69,4 +112,25 @@ class Gen extends Proc {
 	private double expDistPdf(double lambda) {
 		return (-1.0) * Math.log(1 - rand.nextDouble()) / lambda;
 	}
+
+	private int findLowQueue(){
+		int nbrInQ = 0;
+		ArrayList<QS> sameLengthQ = new ArrayList<QS>();
+
+		for (QS qs : queueList) {
+			if (nbrInQ <= qs.inQueue.size()){
+				sameLengthQ.add(qs);
+
+				// TODO HERE
+			}	
+		}
+
+	return
+	}
+
+	public QS getRandomElement(List<QS> list)
+    {
+        Random rand = new Random();
+        return list.get(rand.nextInt(list.size()));
+    }
 }
--- assignments/assign1/task5/MainSimulation.java
+++ assignments/assign1/task5/MainSimulation.java
@@ -1,118 +1,118 @@
-// package assign1.task5;
+package assign1.task5;
 
-// import java.util.*;
-// import java.io.*;
-// import java.nio.charset.StandardCharsets;
-// import java.text.DecimalFormat;
+import java.util.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 
-// //Denna klass �rver Global s� att man kan anv�nda time och signalnamnen utan punktnotation
-// //It inherits Proc so that we can use time and the signal names without dot notation
+//Denna klass �rver Global s� att man kan anv�nda time och signalnamnen utan punktnotation
+//It inherits Proc so that we can use time and the signal names without dot notation
 
-// public class MainSimulation extends Global {
+public class MainSimulation extends Global {
 
-// 	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
-// 		int numberOfSim = 1000;
+		int numberOfSim = 1000;
 
-// 		runSim(1, 1);
-// 		// writeFile(numberOfSim);
+		runSim(1, 1);
+		// writeFile(numberOfSim);
 
-// 	}
+	}
 
-// 	public static ArrayList<Double> runSim(int nrOfSim, int type) {
+	public static ArrayList<Double> runSim(int nrOfSim, int type) {
 
-// 		ArrayList<Double> avgTimeCustomerA = new ArrayList<>();
-// 		ArrayList<Double> avgTimeCustomerB = new ArrayList<>();
+		ArrayList<Double> avgTimeCustomerA = new ArrayList<>();
+		ArrayList<Double> avgTimeCustomerB = new ArrayList<>();
 
-// 		// int x = 0;
-// 		// do {
+		// int x = 0;
+		// do {
 
-// 		// Q5.sendTo = null;
-// 		// Q4.sendTo = null;
-// 		// Q3.sendTo = null;
-// 		// Q2.sendTo = null;
-// 		// Q1.sendTo = null;
+		// Q5.sendTo = null;
+		// Q4.sendTo = null;
+		// Q3.sendTo = null;
+		// Q2.sendTo = null;
+		// Q1.sendTo = null;
 
-// 		Signal actSignal;
-// 		new SignalList();
-// 		boolean run = true;
-// 		Gen Generator = new Gen();
-// 		Generator.lambda = 1;
+		Signal actSignal;
+		new SignalList();
+		boolean run = true;
+		Gen Generator = new Gen();
+		Generator.lambda = 1;
 
-// 		SignalList.SendSignal(RANDOM, Generator, time);
-// 		// SignalList.SendSignal(MEASURE, Q1, time);
+		SignalList.SendSignal(RANDOM, Generator, time);
+		// SignalList.SendSignal(MEASURE, Q1, time);
 
-// 		while (run) {
 
-// 			actSignal = SignalList.FetchSignal();
-// 			time = actSignal.arrivalTime;
-// 			actSignal.destination.TreatSignal(actSignal);
-// 			if (actSignal.signalType == END_ARRIVALS) {
-// 				run = false;
-// 			}
+		while (run) {
+			actSignal = SignalList.FetchSignal();
+			time = actSignal.arrivalTime;
+			actSignal.destination.TreatSignal(actSignal);
+			if (actSignal.signalType == END_ARRIVALS) {
+				run = false;
+			}
 
-// 		}
+		}
 
-// 		// avgTimeCustomerA.addAll(genTimeList(Q1.doneQueueA));
-// 		// avgTimeCustomerB.addAll(genTimeList(Q1.doneQueueB));
+		// avgTimeCustomerA.addAll(genTimeList(Q1.doneQueueA));
+		// avgTimeCustomerB.addAll(genTimeList(Q1.doneQueueB));
 
-// 		// x++;
-// 		// } while (x < nrOfSim);
+		// x++;
+		// } while (x < nrOfSim);
 
-// 		ArrayList<Double> statArr = new ArrayList<>(); /// part/A/B
+		ArrayList<Double> statArr = new ArrayList<>(); /// part/A/B
 
-// 		statArr.add(arrAvg(avgTimeCustomerA));
-// 		statArr.add(arrAvg(avgTimeCustomerB));
+		statArr.add(arrAvg(avgTimeCustomerA));
+		statArr.add(arrAvg(avgTimeCustomerB));
 
-// 		return statArr;
+		return statArr;
 
-// 	}
+	}
 
-// 	// public static ArrayList<Double> writeFile(int nrOfSim) throws
-// 	// InterruptedException, IOException {
+	// public static ArrayList<Double> writeFile(int nrOfSim) throws
+	// InterruptedException, IOException {
 
-// 	// final DecimalFormat df = new DecimalFormat("0.0");
+	// final DecimalFormat df = new DecimalFormat("0.0");
 
-// 	// File file = new File("../task_4_1000Arrivals.txt");
-// 	// FileWriter fw = new FileWriter(file, StandardCharsets.UTF_8);
+	// File file = new File("../task_4_1000Arrivals.txt");
+	// FileWriter fw = new FileWriter(file, StandardCharsets.UTF_8);
 
-// 	// for (double i = 0.1; i <= 0.9; i = i + 0.1) {
-// 	// ArrayList<Double> arr = runSim(nrOfSim, i);
+	// for (double i = 0.1; i <= 0.9; i = i + 0.1) {
+	// ArrayList<Double> arr = runSim(nrOfSim, i);
 
-// 	// double A = arr.get(1);
-// 	// double B = arr.get(2);
+	// double A = arr.get(1);
+	// double B = arr.get(2);
 
-// 	// if (Double.isNaN(arr.get(1)))
-// 	// A = 0;
+	// if (Double.isNaN(arr.get(1)))
+	// A = 0;
 
-// 	// if (Double.isNaN(arr.get(2)))
-// 	// B = 0;
+	// if (Double.isNaN(arr.get(2)))
+	// B = 0;
 
-// 	// fw.write(df.format(arr.get(0)) + " " + df.format(A) + " " + df.format(B) +
-// 	// "\r\n");
-// 	// System.out.println(df.format(arr.get(0)) + " " + df.format(A) + " " +
-// 	// df.format(B));
+	// fw.write(df.format(arr.get(0)) + " " + df.format(A) + " " + df.format(B) +
+	// "\r\n");
+	// System.out.println(df.format(arr.get(0)) + " " + df.format(A) + " " +
+	// df.format(B));
 
-// 	// }
-// 	// fw.close();
+	// }
+	// fw.close();
 
-// 	// return null;
-// 	// }
+	// return null;
+	// }
 
-// 	private static double arrAvg(ArrayList<Double> list) {
-// 		double total = 0;
-// 		for (int i = 0; i < list.size(); i++)
-// 			total = total + list.get(i);
-// 		return (total / list.size());
-// 	}
+	private static double arrAvg(ArrayList<Double> list) {
+		double total = 0;
+		for (int i = 0; i < list.size(); i++)
+			total = total + list.get(i);
+		return (total / list.size());
+	}
 
-// 	private static ArrayList<Double> genTimeList(ArrayList<Customer> list) {
-// 		ArrayList<Double> timeList = new ArrayList<>();
-// 		for (Customer customer : list) {
-// 			timeList.add(customer.getTimeInQueue());
-// 		}
-// 		return timeList;
+	private static ArrayList<Double> genTimeList(ArrayList<Customer> list) {
+		ArrayList<Double> timeList = new ArrayList<>();
+		for (Customer customer : list) {
+			timeList.add(customer.getTimeInQueue());
+		}
+		return timeList;
 
-// 	}
+	}
 
-// }
+}
